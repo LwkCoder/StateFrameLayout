0b8e49d555305814536bac48884a63d971e2a7da
luowenkang <505515031@qq.com>
2018-10-22 14:18:05 +0800

    release v1.1.2 适配AndroidX

--- app/src/main/java/com/lwkandroid/statelayoutdemo/MainActivity.java
+++ app/src/main/java/com/lwkandroid/statelayoutdemo/MainActivity.java
@@ -1,12 +1,13 @@
 package com.lwkandroid.statelayoutdemo;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.lwkandroid.widget.stateframelayout.StateFrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
